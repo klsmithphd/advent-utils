44f4c67b23f5644d98e76f42d8654edc71052e5a
Ken Smith <ken.smith@2scientists.net>
2020-12-19 15:32:32 -0700

    Change default release tasks

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject advent-utils "0.1.1"
+(defproject advent-utils "0.1.1-SNAPSHOT"
   :description "Helper utilities supporting Advent of Code solutions"
   :url "https://github.com/Ken-2scientists/advent-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,4 +8,12 @@
                  [org.clojure/math.combinatorics "0.1.6"]]
   :repl-options {:init-ns advent-utils.core}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://repo.clojars.org"}]
-                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org"}]])
+                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org"}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
